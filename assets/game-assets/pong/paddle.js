47246625328b93e78c305125714f3441edf0f8c9
Flávio Ferreira <63963975+LDKid@users.noreply.github.com>
2022-06-21 23:19:04 +0100

    Version 0.1.1
    Pressing the play button while with popup open now closes the popup.
    Pressing the open popup button while playing now pauses the game.
    Attempt to fix issues related to facebook browser on iOS.

--- assets/game-assets/pong/paddle.js
+++ assets/game-assets/pong/paddle.js
@@ -1,7 +1,7 @@
 class Paddle {
     constructor( context ) {
         this._ctx = context;
-        this.size = { x: 15, y: 200 };
+        this.size = { x: 15, y: 175 };
         this.position = { x: window.innerWidth - this.size.x - 15, y: window.innerHeight/2 - this.size.y/2};
         
     }
